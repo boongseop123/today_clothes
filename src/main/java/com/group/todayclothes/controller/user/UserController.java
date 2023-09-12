0e55088739a18af2b57a8b90c273a2de60436e3c
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-09-12 22:36:23 +0900

    [ADD]회원가입 controller 생성

--- src/main/java/com/group/todayclothes/controller/user/UserController.java
+++ src/main/java/com/group/todayclothes/controller/user/UserController.java
@@ -1,11 +1,22 @@
 package com.group.todayclothes.controller.user;
 
 
+import com.group.todayclothes.dto.user.UserDTO;
+import com.group.todayclothes.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+
 public class UserController {
+    private final UserService userService;
 
-
+    @PostMapping("/user")
+    public void registerUser(@RequestBody UserDTO userDTO){
+        userService.registerUser(userDTO);
+    }
 
 }
