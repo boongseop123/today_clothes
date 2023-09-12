9b82dccc39bb9cf9507781a11822b52d116ad5b4
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-09-12 20:57:18 +0900

    [ADD]Entity클래스 jpa인터페이스 생성

---
+++ src/main/java/com/group/todayclothes/entity/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.group.todayclothes.entity.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//jpa인터베이스 상속--><entity클래스와 PK타입>
+public interface UserRepository extends JpaRepository<User, Long> {
+}
