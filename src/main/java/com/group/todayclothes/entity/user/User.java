55b4725829d4a81fbd40457dfcf1df6e65738126
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-09-12 20:47:40 +0900

    [ADD]회원가입 Entity클래스 생성

--- src/main/java/com/group/todayclothes/entity/user/User.java
+++ src/main/java/com/group/todayclothes/entity/user/User.java
@@ -1,38 +1,50 @@
 package com.group.todayclothes.entity.user;
-//user객체를 만들어서 실제로 저장
+
+import lombok.Builder;
+
+import javax.persistence.*;
+
+@Entity//테이블과 링크될 클래스를 의미
+//@Getter-->클래스 내 모든 필드의 getter 메소드 자동 생성
 public class User {
-    private String id;
-    private String password;
-    private Integer age;
-    private String name;
+    @Id//해당 테이블의 pk필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//pk생성규칙-auto_increment
+    private Long id;
+    @Column(name="user_id")
+    private String userId;
+    @Column(name="user_pw")
+    private String userPwd;
+    @Column(name="user_gender")
     private String gender;
+    @Column(name="user_age")
+    private Integer age;
 
-    public User(String id, String password, Integer age, String name, String gender){
-        if(id==null||id.isBlank()){
-            throw new IllegalArgumentException(String.format("잘못된 id(%s)형식입니다",id));
-        }
-        this.id=id;
-        this.password=password;
-        this.age=age;
-        this.name=name;
-        this.gender=gender;
-    }
+    protected User(){}//기본생성자 필요한 이유-공부하기//@NoArgsConstructor
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public String getPassword() {
-        return password;
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public String getGender() {
+        return gender;
     }
 
     public Integer getAge() {
         return age;
     }
-    public String getName(){
-        return name;
-    }
-    public String gener(){
-        return gender;
+    @Builder//빌더클래스/생성자 주입--settter와 차이점은?
+    public User(String userId, String userPwd, String gender, Integer age ){
+        this.userId=userId;
+        this.userPwd=userPwd;
+        this.age=age;
+        this.gender=gender;
     }
 }
