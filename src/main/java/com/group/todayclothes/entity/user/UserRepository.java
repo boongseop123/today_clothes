0ef0d9a680d9c692c8ec98f8030301fd38e0ccf3
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-09-14 18:58:19 +0900

    [ADD]로그인 로직 구현

--- src/main/java/com/group/todayclothes/entity/user/UserRepository.java
+++ src/main/java/com/group/todayclothes/entity/user/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //jpa인터베이스 상속--><entity클래스와 PK타입>
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserIdAndUserPwd(String userId, String userPwd);
 }
